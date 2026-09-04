9209c353a1da92dfb9f2495d730d39aedd1f11d4
aacebo <aacebo@gmail.com>
2026-09-03 22:52:54 -0400

    create project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2026web3.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
